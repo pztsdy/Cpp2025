00d53efa882e07e10e26f909e5afc018e693553c
黄郑睿 <Piaoztsdy@outlook.com>
2025-01-16 21:17:22 +0800

    20250116 2117
    A important UPDATE!!!

--- Algorithm.hpp
+++ Algorithm.hpp
@@ -1,1 +1,150 @@
 #include "define.h"
+#ifndef ALGORITHM_HPP
+#define ALGORITHM_HPP
+#endif 
+
+
+// Algorithm
+int factorial(int n)
+{
+    if (n <= 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int fibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int Pow(int x, int n)
+{
+    if (n == 0)
+        return 1;
+    else if (n % 2 == 0)
+    {
+        int y = Pow(x, n / 2);
+        return y * y;
+    }
+    else
+    {
+        return x * Pow(x, n - 1);
+    }
+}
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+int lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+#include <vector>
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    std::vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= n; ++i)
+    {
+        if (is_prime[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                is_prime[j] = false;
+            }
+        }
+    }
+    return is_prime[n];
+}
+
+std::vector<int> sieve_prime(int n)
+{
+    std::vector<bool> is_prime(n + 1, true);
+    std::vector<int> primes;
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= n; ++i)
+    {
+        if (is_prime[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
+                is_prime[j] = false;
+            }
+        }
+    }
+    for (int i = 2; i <= n; ++i)
+    {
+        if (is_prime[i])
+        {
+            primes.push_back(i);
+        }
+    }
+    return primes;
+}
+
+int binarySearch(const std::vector<int> &arr, int target)
+{
+    int left = 0, right = arr.size() - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - right) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
+
+int quickSelect(std::vector<int> &arr, int left, int right, int k)
+{
+    if (left == right)
+        return arr[left];
+
+    int pivotIndex = left + rand() % (right - left + 1);
+    pivotIndex = partition(arr, left, right, pivotIndex);
+
+    if (k == pivotIndex)
+        return arr[k];
+    else if (k < pivotIndex)
+        return quickSelect(arr, left, pivotIndex - 1, k);
+    else
+        return quickSelect(arr, pivotIndex + 1, right, k);
+}
+
+int selectKthSmallest(std::vector<int> &arr, int k)
+{ // support for k = 0, 1, 2, ...
+    return quickSelect(arr, 0, arr.size() - 1, k);
+}
+
+int partition(std::vector<int> &arr, int left, int right, int pivotIndex)
+{
+    int pivotValue = arr[pivotIndex];
+    std::swap(arr[pivotIndex], arr[right]);
+    int storeIndex = left;
+
+    for (int i = left; i < right; ++i)
+    {
+        if (arr[i] < pivotValue)
+        {
+            std::swap(arr[i], arr[storeIndex]);
+            ++storeIndex;
+        }
+    }
+    std::swap(arr[storeIndex], arr[right]);
+    return storeIndex;
+}
---
+++ examples/demo3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include "Algorithm.hpp" // 假设这些函数在Algorithm.hpp中定义
+
+using namespace std;
+
+int main()
+{
+    int n, a[13] = {0, 4, 1, 12, 9, 6, 5, 3, 2, 7, 8, 10, 11}; // 修正数组大小为13
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "factorial(" << n << ") = " << factorial(n) << endl;
+    cout << "fibonacci(" << n << ") = " << fibonacci(n) << endl;
+    cout << "Pow(2, " << n << ") = " << Pow(2, n) << endl;
+    cout << "gcd(12, " << n << ") = " << gcd(12, n) << endl;
+    cout << "lcm(12, " << n << ") = " << lcm(12, n) << endl;
+    cout << "isPrime(" << n << ") = " << isPrime(n) << endl;
+    vector<int> primes = sieve_prime(n);
+    cout << "Primes less than or equal to " << n << ": ";
+    for (int i = 0; i < primes.size(); ++i)
+    {
+        cout << primes[i] << " ";
+    }
+    cout << endl;
+    cout << "Binary search for " << n << " in the array: ";
+    for (int i = 0; i < 13; ++i)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    vector<int> vec(a, a + 13);
+    cout << " = " << binarySearch(vec, n) << endl; // 修正binarySearch函数调用，移除多余的参数
+    cout << "Quick select for " << n << " in the array: ";
+    for (int i = 0; i < 13; ++i)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    vector<int> vec(a, a + 13);
+    cout << " = " << quickSelect(vec, 0, 12, n) << endl;
+
+    return 0;
+}
