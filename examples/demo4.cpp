0f7dea6600797fffeaef7655ebb12e7b6befec4c
黄郑睿 <Piaoztsdy@outlook.com>
2025-02-09 15:54:32 +0800

    20250209 1554

--- examples/demo4.cpp
+++ examples/demo4.cpp
@@ -1,23 +1,55 @@
 #include "..\wapi.hpp"
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main() {
-    // It's have some error. Can not "显示窗口"
-
     // 创建两个窗口句柄
-    HWND window1 = CreateWindowHandle("WINDOW1", 90, 90, 120, 240);
-    HWND window2 = CreateWindowHandle("WINDOW2", 90, 90, 120, 240);
+    HWND window1 = CreateWindowExW(
+        0,                              // Optional window styles.
+        L"STATIC",                      // Window class
+        L"Window 1",                    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        GetModuleHandle(NULL),  // Instance handle
+        NULL        // Additional application data
+    );
+
+    HWND window2 = CreateWindowExW(
+        0,                              // Optional window styles.
+        L"STATIC",                      // Window class
+        L"Window 2",                    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        GetModuleHandle(NULL),  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (window1 == NULL || window2 == NULL) {
+        cout << "Failed to create windows" << endl;
+        return 1;
+    }
+
+    // Show the windows
+    ShowWindow(window1, SW_SHOW);
+    ShowWindow(window2, SW_SHOW);
 
     // 修改第一个窗口的标题
     cout << "change window1 title" << endl;
-    SetWindowTextA(window1, "Hello, World!");
+    SetWindowTextW(window1, L"Hello, World!");
 
     std::cout << "press any key to continue..." << std::endl;
     cin.get();
     return 0;
 }
-
-
-// test
